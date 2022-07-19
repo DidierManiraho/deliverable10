24f16f12291b5effa5247fb27ee0166d21892182
Didier Maniraho <maniraho.didier2190@gmail.com>
2022-07-19 13:35:45 -0400

    Adding javascript functionality

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 
 
+  function bold() {
+    document.getElementById("textarea1").style.fontWeight = "bold";
+  }
+  function italic() {
+    document.getElementById("textarea1").style.fontStyle = "italic";
+  }
+  function left() {
+    document.getElementById("textarea1").style.textAlign = "left";
+  }
+  function right() {
+    document.getElementById("textarea1").style.textAlign = "right";
+  }
+  function center() {
+    document.getElementById("textarea1").style.textAlign = "center";
+  }
+  function upper() {
+    document.getElementById("textarea1").style.textTransform = "uppercase";
+  }
+  function lower() {
+    document.getElementById("textarea1").style.textTransform = "lowercase";
+  }
+  function capital() {
+    document.getElementById("textarea1").style.textTransform = "capitalize";
+  }
+  function clear() {
+    document.getElementById("textarea1").value = "";
+  }
